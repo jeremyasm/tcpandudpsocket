4ac3d9f2564e3ddea815164c0885162e2299bd3d
Li Yaoxing <bjyxli@cn.ibm.com>
2016-07-25 00:28:25 +0800

    fix UDPClient

--- TCPandUDPSocket/src/network/UDPClient.java
+++ TCPandUDPSocket/src/network/UDPClient.java
@@ -1,28 +1,31 @@
 package network;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
+import java.net.SocketException;
 
 public class UDPClient {
-	BufferedReader inFromUser = new BufferedReader(new InputStreamReader(System.in));
-	DatagramSocket clientSocket = new DatagramSocket();
-	InetAddress IPAddress = InetAddress.getByName("hostname");
-	byte[] sendData = new byte[1024];
-	byte[] receiveData = new byte[1024];
-	String sentence = inFromUser.readLine();
-	sendData = sentence.getBytes();
-	DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, IPAddress, 9876);
-	clientSocket.send(sendPacket);
-	DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
-	clientSocket.receive(receivePacket);
-	String modifiedSentence = new String(receivePacket.getData());
-	System.out.println("FROM SERVER: " + modifiedSentence );
-	clientSocket.close();
+	
+	public static void main(String[] args) throws IOException{
+		
+		BufferedReader inFromUser = new BufferedReader(new InputStreamReader(System.in));
+		DatagramSocket clientSocket = new DatagramSocket();
+		InetAddress IPAddress = InetAddress.getByName("hostname");
+		byte[] sendData = new byte[1024];
+		byte[] receiveData = new byte[1024];
+		String sentence = inFromUser.readLine();
+		sendData = sentence.getBytes();
+		DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, IPAddress, 9876);
+		clientSocket.send(sendPacket);
+		DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
+		clientSocket.receive(receivePacket);
+		String modifiedSentence = new String(receivePacket.getData());
+		System.out.println("FROM SERVER: " + modifiedSentence );
+		clientSocket.close();
+	}
 
-	
-	
-	
 }
